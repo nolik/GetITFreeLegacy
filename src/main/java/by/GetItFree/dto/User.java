188336ecdbf691a5d992eb998dba6cdc65e7b3f6
nolik <novik03@mgail.com>
2017-01-07 20:53:29 +0300

    Initial commit for the GetItFree Project.

---
+++ src/main/java/by/GetItFree/dto/User.java
@@ -0,0 +1,12 @@
+package main.java.by.GetItFree.dto;
+
+/**
+ * DTO class of User entity.
+ *
+ * Created by nolik on 07.01.17.
+ */
+public class User {
+    private String login;
+    private String password;
+    private String role;
+}
