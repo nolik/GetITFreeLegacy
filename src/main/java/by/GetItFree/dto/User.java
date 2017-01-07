f0071bf07695b00225acdbf76576b76810bfe64c
nolik <novik03@mgail.com>
2017-01-07 21:28:11 +0300

    Add gitignore file and maven.

--- src/main/java/by/GetItFree/dto/User.java
+++ src/main/java/by/GetItFree/dto/User.java
@@ -1,4 +1,4 @@
-package main.java.by.GetItFree.dto;
+package by.GetItFree.dto;
 
 /**
  * DTO class of User entity.
@@ -9,4 +9,47 @@ public class User {
     private String login;
     private String password;
     private String role;
+    private String email;
+
+    public User(String login, String password, String role, String email) {
+        this.login = login;
+        this.password = password;
+        this.role = role;
+        this.email = email;
+    }
+
+    public User() {
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
--- web/index.jsp
+++ web/index.jsp
@@ -7,10 +7,10 @@
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
-  <head>
+<head>
     <title>$Title$</title>
-  </head>
-  <body>
-  Hello here will be future Get It Free portal.
-  </body>
+</head>
+<body>
+Hello here will be future Get It Free portal.
+</body>
 </html>
